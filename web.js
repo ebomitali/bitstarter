631a73aafbf9fec1a00a6abb81d5257b29150efa
Evelino Bomitali <e.bomitali@gmail.com>
2013-07-10 22:01:58 +0000

    Updated web.js with read file

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
+var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html',function (err, data) {
+  if (err) throw err;
+  console.log(data);
+}))
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString);
 });
 
 var port = process.env.PORT || 5000;
